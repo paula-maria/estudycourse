b4380d511ca5359f512055001450e905b5f8acef
Paula Maria <paulamaria.loc@gmail.com>
2026-08-04 10:49:40 -0300

    feat: conversa com o banco ao criar o user repository

---
+++ backend/app/users/repository.py
@@ -0,0 +1,69 @@
+"""
+Repository de usuários.
+
+Responsabilidade:
+
+- Fazer consultas no banco;
+- Criar usuários;
+- Buscar usuários existentes.
+
+Não contém regras de negócio.
+"""
+
+from sqlalchemy.orm import Session
+
+from app.users.model import User
+
+
+def get_user_by_email(
+    db: Session,
+    email: str
+):
+    """
+    Busca um usuário pelo email.
+
+    Usado para:
+
+    - verificar se email já existe;
+    - autenticação no login.
+    """
+
+    return (
+        db.query(User)
+        .filter(User.email == email)
+        .first()
+    )
+
+
+
+def create_user(
+    db: Session,
+    user: User
+):
+    """
+    Salva um usuário no banco.
+
+    Recebe:
+
+    User(
+        name,
+        email,
+        password_hash
+    )
+
+    Executa:
+
+    INSERT INTO users
+
+    Retorna:
+
+    usuário criado com id.
+    """
+
+    db.add(user)
+
+    db.commit()
+
+    db.refresh(user)
+
+    return user
