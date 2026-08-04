c4faddb4c7693d506bbbbc9babeab4ef32976fcb
Paula Maria <paulamaria.loc@gmail.com>
2026-08-04 10:52:18 -0300

    feat: criação do serviço do user

---
+++ backend/app/users/service.py
@@ -0,0 +1,83 @@
+"""
+Service de usuários.
+
+Responsabilidade:
+
+- Aplicar regras de negócio;
+- Coordenar criação de usuários;
+- Usar segurança de senha;
+- Chamar repository.
+
+Não faz SQL diretamente.
+"""
+
+from sqlalchemy.orm import Session
+
+from app.users import repository
+from app.users.model import User
+from app.users.schemas import UserCreate
+from app.core.security import hash_password
+
+
+
+def create_user(
+    db: Session,
+    user_data: UserCreate
+):
+    """
+    Cria um novo usuário.
+
+    Fluxo:
+
+    UserCreate
+        |
+        |
+        v
+    Verifica email
+        |
+        |
+        v
+    Gera password_hash
+        |
+        |
+        v
+    Cria Model User
+        |
+        |
+        v
+    Repository salva no banco
+    """
+
+
+    # Verifica se já existe usuário
+    existing_user = repository.get_user_by_email(
+        db,
+        user_data.email
+    )
+
+
+    if existing_user:
+        raise ValueError(
+            "Email já cadastrado"
+        )
+
+
+    # Transformar senha em hash
+    password_hash = hash_password(
+        user_data.password
+    )
+
+
+    # Criar objeto SQLAlchemy
+    new_user = User(
+        name=user_data.name,
+        email=user_data.email,
+        password_hash=password_hash
+    )
+
+
+    # Salvar no banco
+    return repository.create_user(
+        db,
+        new_user
+    )
